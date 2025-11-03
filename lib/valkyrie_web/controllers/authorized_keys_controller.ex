e0432f796da31643f6e82ac9039526a7d7c9e34d
Lukas Güldenstein <git@lukasgueldenstein.de>
2025-11-03 19:48:44 +0100

    Adds serving authorized keys and the signature

---
+++ lib/valkyrie_web/controllers/authorized_keys_controller.ex
@@ -0,0 +1,54 @@
+defmodule ValkyrieWeb.AuthorizedKeysController do
+  use ValkyrieWeb, :controller
+
+  alias Valkyrie.Members.Member
+
+  @doc """
+  Serves the authorized_keys file with SSH public keys from all members.
+  """
+  def authorized_keys(conn, _params) do
+    conn
+    |> put_resp_header("content-type", "application/octet-stream")
+    |> text(build_authorized_keys_content())
+  end
+
+  @doc """
+  Builds the signature for the authorized_keys content.
+  """
+  def authorized_keys_signature(conn, _params) do
+    signature =
+      build_authorized_keys_content()
+      |> sign()
+
+    conn
+    |> put_resp_header("content-type", "application/pgp-signature")
+    |> text(signature)
+  end
+
+  defp build_authorized_keys_content() do
+    Ash.read!(Member)
+    |> Enum.sort_by(fn member -> member.tree_name end)
+    # |> Enum.filter(fn %Member{} = m -> m.is_active end)
+    |> Enum.filter(fn %Member{} = m -> m.has_key end)
+    |> Enum.filter(fn %Member{} = m -> Member.ssh_public_key_valid?(m.ssh_public_key) end)
+    |> Enum.map(&get_ssh_pub_key_for_list/1)
+    |> Enum.join("\n")
+  end
+
+  defp get_ssh_pub_key_for_list(%{ssh_public_key: nil}), do: nil
+  defp get_ssh_pub_key_for_list(%{ssh_public_key: ""}), do: nil
+
+  defp get_ssh_pub_key_for_list(member) do
+    [proto, core | _] = String.split(member.ssh_public_key, " ")
+    "#{proto} #{core} #{member.tree_name}"
+  end
+
+  defp sign(binary) do
+    priv_key =
+      Application.fetch_env!(:valkyrie, :xdoor_signing_key)
+      |> ExPublicKey.loads!()
+
+    {:ok, signature} = ExPublicKey.sign(binary, priv_key)
+    Base.encode64(signature)
+  end
+end
